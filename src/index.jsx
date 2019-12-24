cba5fa8b0f70f6ba34e103b1938810f64b5c487e
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2019-12-24 10:42:58 +0900

    created relative reducers

--- src/index.jsx
+++ src/index.jsx
@@ -7,19 +7,27 @@ import { createStore, combineReducers } from 'redux';
 // internal modules
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
+import MessagesReducer from './reducers/messages_reducer';
+import ChannelsReducer from './reducers/channels_reducer';
+import SelectedChannelReducer from './reducers/selected_channel_reducer';
+import CurrentUsernameReducer from './reducers/current_username_reducer';
+
+// Initial state
+// const initialState = {
+//   messages: [],
+//   channels: ['general', 'react', 'tokyo'],
+//   selectedChannel: 'general',
+//   currentUsername: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`
+// };
 
 // State and reducers
 const reducers = combineReducers({
-  changeMe: (state = null, action) => state
+  messages: MessagesReducer,
+  channels: ChannelsReducer,
+  selectedChannel: SelectedChannelReducer,
+  currentUsername: CurrentUsernameReducer
 });
 
-// Initial state
-const initialState = {
-  messages: [],
-  channels: ['general', 'react', 'tokyo'],
-  selectedChannel: 'general',
-  currentUsername: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`
-};
 
 // render an instance of the component in the DOM
 ReactDOM.render(
